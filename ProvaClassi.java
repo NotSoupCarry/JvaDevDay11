29d1d4376ea2e05fd34820841392baa37ca7cbae
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-03-03 10:52:36 +0100

    Update ProvaClassi.java

--- ProvaClassi.java
+++ ProvaClassi.java
@@ -1,4 +1,3 @@
-
 public class ProvaClassi {
 
      public static void main(String[] args){
@@ -14,34 +13,34 @@ public class ProvaClassi {
      }
 }
 
-class Auto {
-    //attributi
-    public String modello;
-    public String colore;
-    public int velocita;
+// class Auto {
+//     //attributi
+//     public String modello;
+//     public String colore;
+//     public int velocita;
 
-    //costruttore
-    public Auto(String modello, String colore){
-        this.modello = modello;
-        this.colore = colore;
-        this.velocita = 0; //Velocità iniziale
-    }
+//     //costruttore
+//     public Auto(String modello, String colore){
+//         this.modello = modello;
+//         this.colore = colore;
+//         this.velocita = 0; //Velocità iniziale
+//     }
 
-    //metodi
-    //metodo per accellerare
-    public void accellera(int incremento){
-        velocita += incremento;
-        System.out.println(modello + ": ha accellerato, velocità attuale: " + velocita + " km/h");
-    }
+//     //metodi
+//     //metodo per accellerare
+//     public void accellera(int incremento){
+//         velocita += incremento;
+//         System.out.println(modello + ": ha accellerato, velocità attuale: " + velocita + " km/h");
+//     }
 
-    //metodo per frenare
-    public void frenare(int decremento){
-        velocita -= decremento;
-        if (velocita < 0){
-            velocita = 0;
-        }
+//     //metodo per frenare
+//     public void frenare(int decremento){
+//         velocita -= decremento;
+//         if (velocita < 0){
+//             velocita = 0;
+//         }
 
-        System.out.println(modello + ": ha frenato, velocità attuale: " + velocita + " km/h");
+//         System.out.println(modello + ": ha frenato, velocità attuale: " + velocita + " km/h");
 
-    }
-}
+//     }
+// }
