445b42acc731efa789844d16955be82502cd97aa
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-03-03 10:05:48 +0100

    Create ProvaClassi.java

---
+++ ProvaClassi.java
@@ -0,0 +1,8 @@
+public class ProvaClassi {
+
+     int x = 5;
+     public static void main(String[] args){
+        ProvaClassi myObj = new ProvaClassi();
+        System.out.println(myObj.x);
+     }
+}
